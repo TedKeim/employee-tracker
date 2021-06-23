6cc03cd228b28fed1b4d0e1aea123a41ba8cdb17
Ted Keim <tedkeim@yahoo.com>
2021-06-22 20:57:19 -0400

    created DB and set up server

--- config/server.js
+++ config/server.js
@@ -4,3 +4,21 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 
+// set up server and port # 
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "password",
+    database: "tracker_DB"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    start();
+  });
+
+  // start inquirer prompt
+
+  // add functions to add role, employee, and department
