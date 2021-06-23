d62b68cf506bd238deff276106595fbbd5a482fc
Ted Keim <tedkeim@yahoo.com>
2021-06-23 12:04:38 -0400

    added update role function and inquirer prompt to update employees

--- config/server.js
+++ config/server.js
@@ -184,3 +184,18 @@ const connection = mysql.createConnection({
           start();
         });
       }
+
+      function updateRole() {
+        const query = "SELECT id, first_name, last_name, role_id  FROM employee";
+        connection.query(query, function(err, res) {
+          if (err) throw err;
+          console.table(res);
+          {
+            inquirer.prompt({
+              type: "input",
+              message: "Which employee needs to be updated? (please use number from id column only)",
+              name: "employee"
+            });
+          }
+        });
+      }
