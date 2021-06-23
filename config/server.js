593658dca8f6466f1a7e9110a4fe8701fe64ec62
Ted Keim <tedkeim@yahoo.com>
2021-06-22 21:11:19 -0400

    added inquirer prompts

--- config/server.js
+++ config/server.js
@@ -20,5 +20,74 @@ const connection = mysql.createConnection({
   });
 
   // start inquirer prompt
+  
+  function start() {
+    inquirer
+      .prompt({
+        type: "list",
+        name: "option",
+        message: "Pick an option!",
+        choices: [
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "View Department",
+          "View Role",
+          "View Employee",
+          "Update Employee Role",
+          "Exit"
+        ]
+      })
 
-  // add functions to add role, employee, and department
+      function addDepartment() {
+        inquirer
+          .prompt({
+            type: "input",
+            message: "What is the name of the department you want to add?",
+            name: "department"
+          })
+
+          function addRole() {
+            inquirer
+              .prompt([
+                {
+                  type: "input",
+                  message: "What is the job title you want to add?",
+                  name: "title"
+                },
+                {
+                  type: "input",
+                  message: "What is the salary for this position?",
+                  name: "salary"
+                },
+                {
+                  type: "input",
+                  message: "What is the department ID for this position?",
+                  name: "departmentID"
+                }
+              ])
+
+              function addEmployee() {
+                inquirer
+                  .prompt([
+                    {
+                      type: "input",
+                      message: "What is the employee's first name?",
+                      name: "firstName"
+                    },
+                    {
+                      type: "input",
+                      message: "What is the employee's last name?",
+                      name: "lastName"
+                    },
+                    {
+                      type: "input",
+                      message: "What is the employee's role ID?",
+                      name: "roleID"
+                    },
+                    {
+                      type: "input",
+                      message: "What is the employee's manager ID?",
+                      name: "managerID"
+                    }
+                  ])
